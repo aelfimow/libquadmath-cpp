54612f483e8bce01febfa7796cc0c746ab72b9dd
Alexander Elfimow <my.free.mail@gmx.de>
2020-06-18 22:38:15 +0200

    Added extern const data

--- src/quadmath.cpp
+++ src/quadmath.cpp
@@ -1,1 +1,34 @@
+#include <quadmath.h>
+
 #include "quadmath_const.h"
+
+namespace quadmath
+{
+
+const __float128 max = FLT128_MAX;
+const __float128 min = FLT128_MIN;
+const __float128 epsilon = FLT128_EPSILON;
+
+const __float128 denorm_min = FLT128_DENORM_MIN;
+const __float128 mant_dig = FLT128_MANT_DIG;
+const __float128 min_exp = FLT128_MIN_EXP;
+const __float128 max_exp = FLT128_MAX_EXP;
+const __float128 dig = FLT128_DIG;
+const __float128 min_10_exp = FLT128_MIN_10_EXP;
+const __float128 max_10_exp = FLT128_MAX_10_EXP;
+
+const __float128 e = M_Eq;
+const __float128 ld2 = M_LOG2Eq;
+const __float128 log2 = M_LOG10Eq;
+const __float128 ln2 = M_LN2q;
+const __float128 ln10 = M_LN10q;
+const __float128 pi = M_PIq;
+const __float128 pi_2 = M_PI_2q;
+const __float128 pi_4 = M_PI_4q;
+const __float128 one_over_pi = M_1_PIq;
+const __float128 two_over_pi = M_2_PIq;
+const __float128 two_over_sqrt_pi = M_2_SQRTPIq;
+const __float128 sqrt_2 = M_SQRT2q;
+const __float128 one_over_sqrt_2 = M_SQRT1_2q;
+
+}
